73b3630f856404f3c2f6fe8e15dc6506d7e458f6
DE LA CRUZ Allan Babida <allanbabida.delacruz@champ.aero>
2019-08-30 18:08:47 +0800

    Add summary data and sample photo.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO TVSERIES(id,title,rating,genre,channel,grade,language) VALUES
-(1,'Breaking Bad','9.9',2,'AMC',1,1),
-(2,'Game of Thrones','9.8',9,'HBO',1,1),
-(3,'Ang Probinsiyano','9.1',2,'ABS-CBN',1,0),
-(4,'Descendants of the Sun','9.3',8,'KBS',1,2),
-(5,'Descendants of the Sun','9.3',8,'KBS',1,2),
-(6,'Descendants of the Sun','9.3',8,'KBS',1,2),
-(7,'Descendants of the Sun','9.3',8,'KBS',1,2),
-(8,'Descendants of the Sun','9.3',8,'KBS',1,2),
-(9,'Descendants of the Sun','9.3',8,'KBS',1,2),
-(10,'Descendants of the Sun','9.3',8,'KBS',1,2);
+INSERT INTO TVSERIES(id,title,rating,genre,channel,grade,language,summary,image) VALUES
+(1,'Breaking Bad','9.9',2,'AMC',1,1,'A 50-year-old chemistry teacher with lung cancer creates the purest meth and becomes the meth king pin.','brba.jpg'),
+(2,'Game of Thrones','9.8',9,'HBO',1,1,'A crippled boy with useless powers becomes king of Westeros.','got,jpg'),
+(3,'Ang Probinsiyano','9.1',2,'ABS-CBN',1,0,'An immortal cop fights crimes with guest stars.','probin.jpg'),
+(4,'Descendants of the Sun','9.3',8,'KBS',1,2,'A man and a woman with the same surname ends up marrying each other after this.','dots.jpg'),
+(5,'This is US','8.1',8,'KBS',1,2,'Mandy Moore becomes mother of triplets with struggles in life.','us.jpg'),
+(6,'Stranger Things','8.5',1,'Netflix',1,2,'A girl has powers and kills the demogorgon','st.jpg'),
+(7,'Thirteen reasons why','7.4',7,'KBS',1,2,'A high school girl commits suicide after sending cassette tapes to classmates.','13rw.jpg'),
+(8,'FRIENDS','10.0',11,'KBS',1,2,'Still in bed at 10, work began at 8. The best!','friends.jpg'),
+(9,'Kadenang Ginto','6.7',8,'KBS',1,2,'Two women compete to become the best meme.','kadgin.jpg'),
+(10,'Big Bang Theory','8.3',11,'KBS',1,2,'An overly intelligent guy struggles to decipher sarcastic comments.','bigbang.jpg');

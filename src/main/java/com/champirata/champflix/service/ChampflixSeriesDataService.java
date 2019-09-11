831e44eca8f2556607e963a198c082474a6a3f84
DE LA CRUZ Allan Babida <allanbabida.delacruz@champ.aero>
2019-09-11 15:58:31 +0800

    Add sample services for streams

--- src/main/java/com/champirata/champflix/service/ChampflixSeriesDataService.java
+++ src/main/java/com/champirata/champflix/service/ChampflixSeriesDataService.java
@@ -1,15 +1,18 @@
 package com.champirata.champflix.service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.champirata.champflix.constant.Genre;
+import com.champirata.champflix.constant.Language;
 import com.champirata.champflix.model.TVSeries;
 import com.champirata.champflix.repository.SeriesRepository;
 
@@ -33,5 +36,121 @@ public class ChampflixSeriesDataService {
 	}
 	
 	
+	//Slicing methods demo: Get data from 2 show review sites and display the top 10 highly rated drama TV series from both sites combined
+	//DB equivalent: Select distinct (title) from tvseries where rating =>8 limit 0, 10;
+	public List<TVSeries> getHighestRatedFromReviewSites(){
+		
+		return new ArrayList<TVSeries>();
+	}
+	
+	//Matching methods demo:
+	//(a) Is there at least one rated G that is highly rated?
+	//(b) Do all the series have a rating of >7?
+	//(c) Is there any TV series with a rating of lower that 5?
+	public boolean match(List<TVSeries> series){
+		
+		return false;
+	}
 
+	//Finding methods demo
+	//(a)At least one Tagalog series that is highly rated
+	public TVSeries find(List<TVSeries> series) {
+		Optional<TVSeries> TVseries = series.stream()
+				.filter(s -> s.getLanguage().equals(Language.FILIPINO) && s.getRating() >= 8.0)
+				.findAny();
+		
+		if(TVseries.isPresent()) {
+			return TVseries.get();
+		}
+		return new TVSeries();
+	}
+	
+	//Reduction operations - terminal operations
+	//Reduce the stream of elements into a single value
+	//For example: compute the sum of all numbers, or 
+	//maxima or minima, eg, lowest rated series
+	//First syntax: Optional<T> reduce(BinaryOperator<T> accumulator) -> returns an Optional, takes a binary operator as parameter, an accumulator that works with 2 stream elements at a time
+	//Sample: Optional<Integer> sum = Arrays.stream(new Integer[]{5,1,3,6}).reduce(Integer::sum);
+	//Sample parallelized: Optional<Integer> sum = Arrays.stream(new Integer[]{5,1,3,6}).parallel.reduce(Integer::sum); //w/o synchronization
+	//Find the highest rated drama series
+	public TVSeries highestRatedByGenre(Genre genre) {
+
+		Optional<TVSeries> series = getAllSeries().stream().filter(s -> s.getGenre().equals(Genre.DRAMA))
+				.reduce((s1, s2) -> s1.getRating() >= s2.getRating() ? s1 : s2);
+		return series.isPresent() ? series.get() : new TVSeries();
+
+		// IMPERATIVE:
+		/*
+		 * TVSeries result = null; List<TVSeries> series = getAllSeries(); for (TVSeries
+		 * TVseries : series) { // Initialize result with first book having rating >=
+		 * 4.5 if (result == null) { if (TVseries.getGenre().equals(Genre.DRAMA)) {
+		 * result = TVseries; } continue; }
+		 * 
+		 * if (TVseries.getGenre().equals(Genre.DRAMA) && result.getRating() >
+		 * TVseries.getRating()) { result = TVseries; } }
+		 * 
+		 * System.out.println("(Imperative) Highest rated Drama: " + result);
+		 */
+	}
+	
+	//Second syntax: T reduce(T identity, BinaryOperator<T> accumulator); if stream is empty, method will return identity (default value)
+	//For example:strings.stream().reduce("",(s1, s2) -> s1 +s 2);
+	
+	//Third syntax: <U> U reduce(U identity, BiFunction<U,? super T, U> accumulator, BinaryOperator<U> combiner); return U for reduce that takes 3 parameters: an identifier, an accumulator function, and a combiner function
+	//For example: strings.stream().reduce(()-> new StringBuilder(), (sb, s) -> sb.append(s), (sb1,sb2)->sb1.append(sb2));
+	
+	static void overloadedReductions() {
+		System.out.println("\noverloadedReductions ... ");
+
+		String[] grades = { "A", "A", "B", "C", "D", "E" };
+
+		String concat1 = Arrays.stream(grades).reduce("", (s1, s2) -> s1 + s2);
+		System.out.println("concat1: " + concat1);
+
+		StringBuilder concat2 = Arrays.stream(grades).reduce(new StringBuilder(), (sb, s) -> sb.append(s),
+				(sb1, sb2) -> sb1.append(sb2));
+		System.out.println("concat2: " + concat2);
+
+	}
+	
+	public static void main(String[] args) {
+		overloadedReductions();
+	}
+	
+	//Collect - collect elements into a container
+	//Mutable reduction
+	
+	
+	
+	
+	
+	 /***** Here We Are Creating A 'Sequential Stream' & Displaying The Result *****/
+	/*
+	 * t1 = System.currentTimeMillis();
+	 * System.out.println("Sequential Stream Count?= " + eList.stream().filter(e ->
+	 * e.getSalary() > 15000).count());
+	 * 
+	 * t2 = System.currentTimeMillis();
+	 * System.out.println("Sequential Stream Time Taken?= " + (t2-t1) + "\n");
+	 */
+    /***** Here We Are Creating A 'Parallel Stream' & Displaying The Result *****/
+	/*
+	 * t1 = System.currentTimeMillis();
+	 * System.out.println("Parallel Stream Count?= " +
+	 * eList.parallelStream().filter(e -> e.getSalary() > 15000).count());
+	 * 
+	 * t2 = System.currentTimeMillis();
+	 * System.out.println("Parallel Stream Time Taken?= " + (t2-t1));
+	 */
+	
+	/*
+	 * Do remember, Parallel Streams must be used only with stateless,
+	 * non-interfering, and associative operations i.e.
+	 * 
+	 * A stateless operation is an operation in which the state of one element does
+	 * not affect another element A non-interfering operation is an operation in
+	 * which data source is not affected An associative operation is an operation in
+	 * which the result is not affected by the order of operands
+	 */
+	
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO TVSERIES(id,title,rating,genre,channel,grade,language,summary,image) VALUES
 (1,'Breaking Bad','9.9',2,'AMC',1,1,'A 50-year-old chemistry teacher with lung cancer creates the purest meth and becomes the meth king pin.','brba.jpg'),
 (2,'Game of Thrones','9.8',9,'HBO',1,1,'A crippled boy with useless powers becomes king of Westeros.','got.jpeg'),
-(3,'Ang Probinsiyano','9.1',2,'ABS-CBN',1,0,'An immortal cop fights crimes with guest stars.','probin.jpg'),
+(3,'Ang Probinsiyano','0',2,'ABS-CBN',1,0,'An immortal cop fights crimes with guest stars.','probin.jpg'),
 (4,'Descendants of the Sun','9.3',8,'KBS',1,2,'A man and a woman with the same surname ends up marrying each other after this.','dots.jpg'),
 (5,'This is US','8.1',8,'KBS',1,2,'Mandy Moore becomes mother of triplets with struggles in life.','us.jpg'),
 (6,'Stranger Things','8.5',1,'Netflix',1,2,'A girl has powers and kills the demogorgon','st.jpg'),

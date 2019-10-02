5e991efa9b0af0a7bac4d92f7adb77459b1e2d78
DE LA CRUZ Allan Babida <allanbabida.delacruz@champ.aero>
2019-10-02 08:38:41 +0800

    Add notes to service class

--- src/main/java/com/champirata/champflix/service/ChampflixSeriesDataService.java
+++ src/main/java/com/champirata/champflix/service/ChampflixSeriesDataService.java
@@ -50,13 +50,15 @@ public class ChampflixSeriesDataService {
 				// DB: Select distinct (title) from reviews where rating >= 6.0 limit 0, 5;
 				// TODO: Print at most 5 DISTINCT reviews with a rating > 6.0
 				// Your codes here
+				//stateless stream functions need not store any state from the elements, ie, it can process each element on its own without having to check other elements
 				.filter(r -> r.getGenre().equals(genre))
+				//stateful stream functions need to check other elements to perform the operation, eg, sort needs to look at all the elements in the stream first before performing the sort
 				.sorted(Comparator.comparing(SeriesReview::getRating).reversed())//sorts the elements of the stream based on a given Comparator
 				//.peek(r -> System.out.println("current element: " + r.getTitle())) //a method that peeks into the current element being processed along the stream, intended to use only for debugging
 				.distinct() //bases the distinction on the .equals() method of the object
 				//.skip(5) //skips a specified number of elements, still processes the skipped elements, just does not passed them down the pipeline
 				.limit(5) // short-circuit intermediate operation, after getting the limited number of elements, immediately continues to the next method in the pipeline
-				//.map(seriesReview -> seriesReview.getTitle()) // 				
+				//.map(seriesReview -> seriesReview.getTitle()) // map changes the elements of	the stream to another element, in this case, the seriesReview elements are mapped to become String of titles			
 				//.forEach(System.out::println); //a terminal stream method that takes a Consumer, to consume the element in the stream, returns void 
 				.collect(Collectors.toList());
 
@@ -70,15 +72,12 @@ public class ChampflixSeriesDataService {
 	 */
 	public boolean isChildSafe(List<TVSeries> series) {
 		// TODO: Provide a simple check if the list does not contain any series that is
-		// rated SPG
-		
-		return series.stream().noneMatch(s->s.getGrade().equals(Grade.SPG));
-		
-	
+		// rated SPG		
+		return series.stream().noneMatch(s->s.getGrade().equals(Grade.SPG));			
 	}
 
 	/*
-	 * Finding operations Short circuit operations that returns an Optional that may
+	 * Finding operations: Short circuit operations that return an Optional that may
 	 * or may not contain the element sought
 	 */
 	public TVSeries find(List<TVSeries> series, Language language) {
